53a7ee6abb3a9a4470f67d6972e23cc23f8bdfa3
Suh Ryn <R@R.local>
2020-03-15 20:40:15 +0900

    add mongoose

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
-"hello my name is Ryn Suh"
 
 const app = express();
 
+const mongoose = require('mongoose');
+
+
+
 const productsRoute = require('./api/routes/products');
 const ordersRoute = require('./api/routes/orders');
+
+
+
+const mongoDBurl = 'mongodb+srv://seolin202:tjfdls0114**@cluster0-4lqvd.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(mongoDBurl, {useNewUrlParser:true, useUnifiedTopology:true})
+    .then(() => console.log('MongDB connected..'))
+    .catch(err => console.log(err.message));
+
+
+
+
 
 app.use('/products', productsRoute);
 app.use('/orders', ordersRoute);
